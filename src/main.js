7f9f4659717cae7366e53ba13e298fa7fa75277b
Joel <joelvanpatten@gmail.com>
2025-08-18 21:00:51 -0700

    asdfsadf

--- src/main.js
+++ src/main.js
@@ -33,6 +33,34 @@ document.addEventListener('DOMContentLoaded', () => {
   if (studyBuddyElement) {
     renderMarkdown('/markdown/StudyBuddyReadme.md', 'studybuddy-markdown');
   }
+
+  const iconContainer = document.getElementById('icon-container');
+  if (iconContainer) {
+    const icons = [
+      'aws-icon.svg',
+      'bootstrap-4-icon.svg',
+      'css-icon.svg',
+      'docker-icon.svg',
+      'javascript-programming-language-icon.svg',
+      'mysql-icon.svg',
+      'node-js-black-icon.svg',
+      'npm-icon.svg',
+      'php-programming-language-icon.svg',
+      'postman-icon.svg',
+      'python-programming-language-icon.svg',
+      'react-js-icon.svg',
+      'typescript-programming-language-icon.svg',
+      'webpack-icon.svg'
+    ];
+
+    icons.forEach(iconFile => {
+      const img = document.createElement('img');
+      img.src = `/images/svg/ColorIcons/${iconFile}`;
+      img.alt = iconFile.replace('.svg', '').replace(/-/g, ' ');
+      img.classList.add('h-12', 'w-12'); // Example size, can be adjusted
+      iconContainer.appendChild(img);
+    });
+  }
 });
 
 menu();
